533866d2fd0e072213c8189807c51cb141fefcd6
tbbilba7 <50131164+tbbilba7@users.noreply.github.com>
2019-05-02 10:32:43 +0200

    algun_otro_proba

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,9 +2,10 @@
 
 <h3>Inicio</h3>
 
-<p style="background-image:url('himalayaRoerich.jpg.jpg');">Esto es una prueba nueva</p>
+<p>Esto es una prueba nueva</p>
 
-<p >Lorem ipsum dolor sit amet consectetur adipiscing elit dis commodo volutpat, morbi risus lacus nibh potenti sed himenaeos hac. Ad erat placerat magna ultrices auctor nisi egestas ante, felis luctus vel orci dis lectus dignissim libero, turpis tincidunt porttitor lobortis posuere cubilia mollis. Volutpat hac nisl justo dictum gravida a lectus mauris habitasse per mollis penatibus magnis, sem dis ullamcorper venenatis id accumsan nostra morbi elementum senectus aenean maecenas.</p>
+<!-- style="background-image:url(proyecto_final/webroot/img/'himalayaRoerich.jpg');" -->
+<p id="nuevo">Lorem ipsum dolor sit amet consectetur adipiscing elit dis commodo volutpat, morbi risus lacus nibh potenti sed himenaeos hac. Ad erat placerat magna ultrices auctor nisi egestas ante, felis luctus vel orci dis lectus dignissim libero, turpis tincidunt porttitor lobortis posuere cubilia mollis. Volutpat hac nisl justo dictum gravida a lectus mauris habitasse per mollis penatibus magnis, sem dis ullamcorper venenatis id accumsan nostra morbi elementum senectus aenean maecenas.</p>
 
 <p>Auctor sagittis donec himenaeos pretium vel arcu habitasse, sollicitudin tristique ultrices senectus ante tellus nisi, aliquam nullam id pulvinar scelerisque quam. Nisl vitae commodo gravida cursus vel platea in suscipit maecenas tellus felis morbi, duis nulla mus erat orci penatibus lacus taciti turpis ultricies ac. Turpis rhoncus odio consequat bibendum lobortis leo cum torquent vitae sem eros cubilia elementum nec, vel maecenas euismod semper himenaeos senectus posuere morbi risus condimentum pretium magna.</p>
 
